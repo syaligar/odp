c15edcdc35d7285ece6ac852f35d636820bf0d49
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-19 12:36:36 +0000

    Deploy OpenDataPlane/odp to github.com/OpenDataPlane/odp.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,87 @@
+var NAVTREE =
+[
+  [ "API Reference Manual", "index.html", [
+    [ "Introduction", "index.html#sec_1", null ],
+    [ "Contact Details", "index.html#contact", null ],
+    [ "API Guide Lines", "api_guide_lines.html", [
+      [ "Introduction", "api_guide_lines.html#introduction", null ],
+      [ "Functional Definition", "api_guide_lines.html#functional", [
+        [ "Naming Conventions", "api_guide_lines.html#naming", null ],
+        [ "Data Types and Use of typedef", "api_guide_lines.html#data_types", null ],
+        [ "Parameter Structure and Validation", "api_guide_lines.html#parameters", null ],
+        [ "Function Names", "api_guide_lines.html#function_name", null ],
+        [ "Getting information", "api_guide_lines.html#getters", [
+          [ "Is / Has", "api_guide_lines.html#is_has", null ],
+          [ "Get", "api_guide_lines.html#get", null ]
+        ] ],
+        [ "Converter Functions", "api_guide_lines.html#converter", null ],
+        [ "Function Calls", "api_guide_lines.html#function_calls", null ],
+        [ "Use of errno", "api_guide_lines.html#errno", null ],
+        [ "Boolean", "api_guide_lines.html#boolean", null ],
+        [ "Success and Failure", "api_guide_lines.html#success", null ],
+        [ "Internal APIs", "api_guide_lines.html#odp_internal", null ],
+        [ "Declaring variables", "api_guide_lines.html#variables", null ]
+      ] ],
+      [ "Implementation Considerations", "api_guide_lines.html#implementation", [
+        [ "Application View vs. Implementation View", "api_guide_lines.html#application_view", null ],
+        [ "Essential functions vs. Extensions", "api_guide_lines.html#essential_functions", null ],
+        [ "ODP DEPRECATE", "api_guide_lines.html#odp_deprecate", null ]
+      ] ],
+      [ "Default behaviours", "api_guide_lines.html#defaults", null ]
+    ] ],
+    [ "Contributing Guide Lines", "contributing.html", null ],
+    [ "Release Management", "release.html", [
+      [ "API Numbering", "release.html#api_numbering", [
+        [ "Generation", "release.html#generation", null ],
+        [ "Major", "release.html#major", null ],
+        [ "Minor", "release.html#minor", null ]
+      ] ],
+      [ "Implementation String", "release.html#implementation_string", null ]
+    ] ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"arch_2arm32-linux_2odp_2api_2abi_2byteorder_8h_source.html",
+"arch_2x86__64-linux_2odp_2api_2abi_2queue_8h_source.html",
+"group__odp__classification.html#ga42a8a8061dcc20665d96b25b0c0241fd",
+"group__odp__compression.html#gaa3991629c7c7aefeb55e96f323a8ae65",
+"group__odp__crypto.html#gga3c943d596daa203f2c71263ad4386ff0ac8c68b6ba7437f58342798b8b9321d49",
+"group__odp__ipsec.html#ga8f4691ebed7c38c7ef02f60540aeaacc",
+"group__odp__packet.html#ga270a0bc27e5037f2836955df0c7bfab3",
+"group__odp__packet.html#gae88bcb7cf28e3ddfbdc2886a55ee86a3",
+"group__odp__packet__io.html#gga4e0c26e40b1166b07142302d12e8467da9e6184e8d605ce199cfa54d6c50153ff",
+"group__odp__scheduler.html#gaca3f3389f35a947facd5f627303778b6",
+"group__odp__timer.html#ga1d1006d0da428f8afd5527b673f777a9",
+"group__odp__traffic__mngr.html#gad0bd0b2f8578650d18cc6403f381ee6d",
+"structodp__crypto__cipher__capability__t.html#a82de72aef0745f9b6b970a32bc19df52",
+"structodp__packet__parse__result__flag__t.html#a50814e562cffda8c65df67d45c7a1d2a",
+"structodp__time__t.html",
+"unionodp__crypto__cipher__algos__t.html#ab79fe8c130d5e675b8c819bc2ca8bc71"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ platform-api-guide/navtreedata.js
@@ -0,0 +1,86 @@
+var NAVTREE =
+[
+  [ "API Reference Manual for linux-generic", "index.html", [
+    [ "Introduction", "index.html#sec_1", null ],
+    [ "Contact Details", "index.html#contact", null ],
+    [ "API Guide Lines", "api_guide_lines.html", [
+      [ "Introduction", "api_guide_lines.html#introduction", null ],
+      [ "Functional Definition", "api_guide_lines.html#functional", [
+        [ "Naming Conventions", "api_guide_lines.html#naming", null ],
+        [ "Data Types and Use of typedef", "api_guide_lines.html#data_types", null ],
+        [ "Parameter Structure and Validation", "api_guide_lines.html#parameters", null ],
+        [ "Function Names", "api_guide_lines.html#function_name", null ],
+        [ "Getting information", "api_guide_lines.html#getters", [
+          [ "Is / Has", "api_guide_lines.html#is_has", null ],
+          [ "Get", "api_guide_lines.html#get", null ]
+        ] ],
+        [ "Converter Functions", "api_guide_lines.html#converter", null ],
+        [ "Function Calls", "api_guide_lines.html#function_calls", null ],
+        [ "Use of errno", "api_guide_lines.html#errno", null ],
+        [ "Boolean", "api_guide_lines.html#boolean", null ],
+        [ "Success and Failure", "api_guide_lines.html#success", null ],
+        [ "Internal APIs", "api_guide_lines.html#odp_internal", null ],
+        [ "Declaring variables", "api_guide_lines.html#variables", null ]
+      ] ],
+      [ "Implementation Considerations", "api_guide_lines.html#implementation", [
+        [ "Application View vs. Implementation View", "api_guide_lines.html#application_view", null ],
+        [ "Essential functions vs. Extensions", "api_guide_lines.html#essential_functions", null ],
+        [ "ODP DEPRECATE", "api_guide_lines.html#odp_deprecate", null ]
+      ] ],
+      [ "Default behaviours", "api_guide_lines.html#defaults", null ]
+    ] ],
+    [ "Contributing Guide Lines", "contributing.html", null ],
+    [ "Release Management", "release.html", [
+      [ "API Numbering", "release.html#api_numbering", [
+        [ "Generation", "release.html#generation", null ],
+        [ "Major", "release.html#major", null ],
+        [ "Minor", "release.html#minor", null ]
+      ] ],
+      [ "Implementation String", "release.html#implementation_string", null ]
+    ] ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"abi-default_2align_8h.html",
+"group__odp__atomic.html#gaa55026c7f953f826ec1d5337388e3600",
+"group__odp__compiler__optim.html#ga388e5edab224f1315443269a61d30617",
+"group__odp__crypto.html#ga2d955782cc97202560c73588a80b4156",
+"group__odp__initialization.html#ga5d33ab81606ee9692f6ec37401c7bfd9",
+"group__odp__locks.html#ga96a7a16e246bd7468cee2a298202f18a",
+"group__odp__packet.html#ga9eb7cff2a7f32d07bcf919d7286509b1",
+"group__odp__packet__io.html#ga8b2c055332aa85e75f7d5aef5e035d41",
+"group__odp__queue.html#gga95fc9e94b89373fe794269c103ab6b49a99e76a25a1501a4729eb1222c7faded7",
+"group__odp__thread.html#ga4602c3ff56ba8b649f83418d94176480",
+"group__odp__traffic__mngr.html#ga52b2c2bfe47fff819476277a849b67e9",
+"spec_2barrier_8h_source.html",
+"structodp__crypto__op__param__t.html#a42cc4920a2c1a81164b7ca7d6dd33e5e",
+"structodp__packet__parse__result__flag__t.html#acb661e5931c10f4b0a7b3e75264c7b80",
+"structodp__timer__pool__info__t.html#a7f960a2cfe6d8671acf5f86c2e18cad8",
+"unionodp__crypto__cipher__algos__t.html#a3d26320c84b5fff891040fdf59ecd49f"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
